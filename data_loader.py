6d55dfee00275242c095f1230a9dac9eb9d53b92
Cameron Bennett <mr.mach33n@gmail.com>
2022-10-02 08:25:27 -0400

    seperated data loader

---
+++ data_loader.py
@@ -0,0 +1,52 @@
+from torchtext.datasets import SST2
+from torch.utils.data import Dataset, DataLoader
+import torchtext.transforms as transforms
+from torch.hub import load_state_dict_from_url
+
+class EngineDataLoader:
+    def __init__(self, batch_size=16, max_seq=256, shuffle=True):
+        ## Custom Parameters
+        self.max_seq = max_seq
+        self.batch_size = batch_size
+        self.bos_idx = 0
+        self.pad_idx = 1
+        self.eos_idx = 2
+
+        ## Using pytorch provided data for now, might want to incorporate custom later. ##
+        self.train_data = SST2(split="train")
+        self.val_data = SST2(split="dev")
+        self.test_data = SST2(split="test")
+
+        self.train_loader = DataLoader(self.train_data, batch_size=batch_size, shuffle=True, collate_fn=self.collate_fn)
+        self.val_loader = DataLoader(self.val_data, batch_size=batch_size, shuffle=True, collate_fn=self.collate_fn)
+        self.test_loader = DataLoader(self.test_data, batch_size=batch_size, shuffle=True, collate_fn=self.collate_fn)
+
+        ## Shared tokenization transformation. Could evolve and make this unique to individuals in future##
+        xlmr_model_path = r"https://download.pytorch.org/models/text/xlmr.sentencepiece.bpe.model"
+        xlmr_vocab_path = r"https://download.pytorch.org/models/text/xlmr.vocab.pt"
+
+        self.tokenize = transforms.SentencePieceTokenizer(xlmr_model_path)
+        self.vocab_transform = transforms.VocabTransform(load_state_dict_from_url(xlmr_vocab_path))
+        self.truncate = transforms.Truncate(max_seq)
+        self.append_token = lambda idx, begin: transforms.AddToken(token=idx, begin=begin)
+        
+
+    def collate_fn(self, inp):
+        return list(map(lambda x: self.transformText(x[0]), inp)), list(map(lambda x: x[1], inp))
+        
+    def transformText(self, text):
+        out = self.tokenize(text)
+        out = self.vocab_transform(out)
+        out = self.truncate(out)
+        out = self.append_token(idx=0, begin=True)(out)
+        out = self.append_token(idx=2, begin=False)(out)
+        return out
+        
+    # Dev Helper functions
+    def outputText(self):
+        for t_id, label in self.train_loader:
+            print(t_id)
+            print(label)
+            print()
+            break
+        
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from torch import cuda, device
-from torch.hub import load_state_dict_from_url
-from torchtext.datasets import SST2
-import torchtext.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 
 # Temporarily ignoring option to build and just relative importing deap right now
 from deap.deap import base, creator, gp, tools
 
+import data_loader as DL
 import layer_methods as LM
 import random
 
@@ -15,13 +12,7 @@ class Engine:
     def __init__(self, batch_size=16, max_seq=256):
         self.toolbox = base.Toolbox()
         self.device = device("cuda" if cuda.is_available() else "cpu")
-
-        ## Custom Parameters
-        self.max_seq = max_seq
-        self.batch_size = batch_size
-        self.bos_idx = 0
-        self.pad_idx = 1
-        self.eos_idx = 2
+        self.dataloader = DL.EngineDataLoader(batch_size=batch_size, max_seq=max_seq, shuffle=True)
 
         ## DEAP Specific
         # default assumes accuracy, latency
@@ -31,29 +22,9 @@ class Engine:
         self.max_size = 10
         self.pop_size = 100
 
-        ## Using pytorch provided data for now, might want to incorporate custom later. ##
-        self.train_data = SST2(split="train")
-        self.val_data = SST2(split="dev")
-        self.test_data = SST2(split="test")
-
-        #print(self.transformBatch(self.train_data))
-
-        self.train_loader = DataLoader(self.train_data, batch_size=batch_size, shuffle=True, collate_fn=self.collate_fn)
-        self.val_loader = DataLoader(self.val_data, batch_size=batch_size, shuffle=True, collate_fn=self.collate_fn)
-        self.test_loader = DataLoader(self.test_data, batch_size=batch_size, shuffle=True, collate_fn=self.collate_fn)
-
-        ## Shared tokenization transformation. Could evolve and make this unique to individuals in future##
-        xlmr_model_path = r"https://download.pytorch.org/models/text/xlmr.sentencepiece.bpe.model"
-        xlmr_vocab_path = r"https://download.pytorch.org/models/text/xlmr.vocab.pt"
-
-        self.tokenize = transforms.SentencePieceTokenizer(xlmr_model_path)
-        self.vocab_transform = transforms.VocabTransform(load_state_dict_from_url(xlmr_vocab_path))
-        self.truncate = transforms.Truncate(max_seq)
-        self.append_token = lambda idx, begin: transforms.AddToken(token=idx, begin=begin)
 
     """ Primary method for running evolutionary search."""
     def run(self):
-        #self.outputText()
         self.setupDEAPTools()
         self.search()
 
@@ -89,14 +60,14 @@ class Engine:
         
         # Sample instantiation of Individual(Debug)
         #expr = gp.genNetInd(self.pset, 7)
-        expr = self.toolbox.individual()
-        print(expr)
-        tree = gp.PrimitiveTree(expr)
-        individual = creator.NetInd([tree])
-        print()
-        print(tree)
-        print()
-        print(individual)
+        #expr = self.toolbox.individual()
+        #print(expr)
+        #tree = gp.PrimitiveTree(expr)
+        #individual = creator.NetInd([tree])
+        #print()
+        #print(tree)
+        #print()
+        #print(individual)
 
     def genPSET(self):
         # All available primitives
@@ -130,26 +101,6 @@ class Engine:
             # Put this off until future
 
             gen += 1
-
-
-    def collate_fn(self, inp):
-        return list(map(lambda x: self.transformText(x[0]), inp)), list(map(lambda x: x[1], inp))
-
-    def transformText(self, text):
-        out = self.tokenize(text)
-        out = self.vocab_transform(out)
-        out = self.truncate(out)
-        out = self.append_token(idx=0, begin=True)(out)
-        out = self.append_token(idx=2, begin=False)(out)
-        return out
-
-    # Dev Helper functions
-    def outputText(self):
-        for t_id, label in self.train_loader:
-            print(t_id)
-            print(label)
-            print()
-            break
 
 engine = Engine()
 engine.run()
